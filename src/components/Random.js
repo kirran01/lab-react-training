9b61d6ef5f1d22fc51a36a440afd5f9e168c09c9
kirran01 <Kirran-@live.com>
2022-12-06 10:37:32 -0500

    it3

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Random = (props) => {
-    return (
-        <div>
-          <p>min={props.min}</p>  
-          <p>min={props.max}</p>
-        </div>
-    );
-}
+  let rando = Math.floor(Math.random() * (props.max - props.min) + props.min);
+  return (
+    <div>
+      <p>
+        random value between {props.min} and {props.max} = {rando}
+      </p>
+    </div>
+  );
+};
 
 export default Random;
