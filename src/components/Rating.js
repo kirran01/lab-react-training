f9e6ca82510ffde2f621b03bf7ba715ddf1652f1
kirran01 <Kirran-@live.com>
2022-12-06 15:36:33 -0500

    it6

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,11 +1,43 @@
 import React from 'react';
 
 const Rating = (props) => {
+  if (Math.ceil(props.children) == 5) {
     return (
-        <div>
-            <p>{props.children}</p>
-        </div>
+      <div>
+        <p>★★★★★</p>
+      </div>
     );
-}
+  } else if (Math.ceil(props.children) == 4) {
+    return (
+      <div>
+        <p>★★★★☆</p>
+      </div>
+    );
+  } else if (Math.ceil(props.children) == 3) {
+    return (
+      <div>
+        <p>★★★☆☆</p>
+      </div>
+    );
+  } else if (Math.ceil(props.children) == 2) {
+    return (
+      <div>
+        <p>★★☆☆☆</p>
+      </div>
+    );
+  } else if (Math.ceil(props.children) == 1) {
+    return (
+      <div>
+        <p>★☆☆☆☆</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>☆☆☆☆☆</p>
+      </div>
+    );
+  }
+};
 
 export default Rating;
