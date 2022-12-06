2f5c10856279aa246ce99cc468b7bedf8bd6a925
kirran01 <Kirran-@live.com>
2022-12-06 16:19:45 -0500

    it7

--- src/components/DriverCard.js
+++ src/components/DriverCard.js
@@ -1,14 +1,36 @@
 import React from 'react';
 
 const DriverCard = (props) => {
-    return (
-        <div>
-            <p>name={props.name}</p>
-            <p>rating={props.rating}</p>
-            <p>img={props.img}</p>
-            <p>car={props.car}</p>
+  let stars = '★'.repeat(Math.round(props.rating));
+  //the amt of blank stars is just the total stars (5) minus the used stars
+  let empties = '☆'.repeat(5 - Math.round(props.rating))
+
+  return (
+    <div className="driver-card">
+      <img
+        style={{ height: `100px`, width: `100px`, borderRadius: `100px` }}
+        src={props.img}
+        alt="1"
+      />
+      <div className="driver-card-info">
+        <div className="driver-card-name">
+          <p>{props.name}</p>
         </div>
-    );
-}
+        <div className="driver-card-rating">
+          <p>
+            {props.rating}
+            {stars}{empties}
+          </p>
+        </div>
+        <div className="model-and-plate">
+          <p>{props.car.model}</p>
+          <p>{props.car.licensePlate} </p>
+        </div>
+      </div>
+
+      {/* <p>car:{props.car}</p> */}
+    </div>
+  );
+};
 
 export default DriverCard;
