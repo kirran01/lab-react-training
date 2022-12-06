82f0de80e8aadc0bfb3bf0a16eca72fd4c8011fe
kirran01 <Kirran-@live.com>
2022-12-06 16:23:24 -0500

    it8

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 
 const LikeButton = (props) => {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <button value={props.likes}>likes</button>
+      <button onClick={()=>{
+        setCount(count+1)
+      }}>{count} likes</button>
     </div>
   );
 };
